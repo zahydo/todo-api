02aec6af13a0a1344210399c4c68271668129702
Santiago Hyun <santiago.hyun@codescrum.com>
2019-08-23 16:06:23 -0500

    Fix problem with database setup, the initial configuration is externally managed when the container is running

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 set -e
-# Setup database for first time
-bundle exec rails db:setup
 
-# Run migrations
-bundle exec rails db:migrate
-
-# Load pre data
-bundle exec rails db:seed
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /app/tmp/pids/server.pid
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
